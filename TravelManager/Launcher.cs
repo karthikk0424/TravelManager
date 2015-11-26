ac3141ca9f90605ddb8f5240430f713f6b5b6d7a
karthik karunakaran <karthikk0424@gmail.com>
2015-11-25 19:42:29 -0500

    Summary : Project creation

---
+++ TravelManager/Launcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TravelManager
+{
+    /*
+     * This is the entry point of the application,
+     * Initialize modules and managers here.
+     */
+    class Launcher
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
