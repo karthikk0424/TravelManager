8d82f255a4fa8b8a4ca56b2b6a3dc694e8bd58fc
Padma <padmagitha@gmail.com>
2015-12-27 01:05:07 +0530

    Changes

--- TravelManager/Constant.cs
+++ TravelManager/Constant.cs
@@ -7,5 +7,19 @@ namespace TravelManager
 {
     class Constant
     {
+        public const string XML_SERVER_PATH = "http://127.0.0.1/LibraryInventory/";
+
+        public const string XML_FILE_NAME_SOURCE = "sourcePt.xml";
+        public const string XML_FILE_NAME_DESTINATION = "destinationPt.xml";
+        public const string XML_FILE_NAME_BUSDETAILS = "bookDt.xml";
+
+        public const string XML_ROOT_ELEMENT_SOURCE = "source";
+        public const string XML_ELEMENT_NODE_SOURCE = "sourcePt";
+
+        public const string XML_ROOT_ELEMENT_DESTINATION = "destination";
+        public const string XML_ELEMENT_NODE_DESTINATION = "destinationPt";
+
+        public const string XML_ROOT_ELEMENT_BUSDETAILS = "busDetail";
+        public const string XML_ELEMENT_NODE_BUSDETAILS = "busDetails";
     }
 }
